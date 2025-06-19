fbad89ce143355d0c31f0ce2334d22467ab4e078
siskaokt <krisdayantisiskaoktavia@gmail.com>
2025-06-19 17:51:01 +0700

    Fixing: Perbaikan Workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,8 +1,12 @@
 # Import library yang digunakan
 import pandas as pd
+import mlflow
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+# Set experiment MLflow
+mlflow.set_experiment("Banknote Experiment")
 
 # Load dataset
 df = pd.read_csv("banknote_preprocessing.csv")
@@ -30,3 +34,12 @@ y_pred = best_model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 prec = precision_score(y_test, y_pred)
 rec = recall_score(y_test, y_pred)
+
+with mlflow.start_run():
+    mlflow.log_param("best_C", grid.best_params_["C"])
+    mlflow.log_param("kernel", grid.best_params_["kernel"])
+    mlflow.log_param("gamma", grid.best_params_["gamma"])
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("precision", prec)
+    mlflow.log_metric("recall", rec)
